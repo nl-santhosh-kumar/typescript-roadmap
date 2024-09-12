497c48b4aa2d5fc5ae4d597e2c54fe33c165538d
santhoshkumarravichandran <santhoshkumar.ravi@gmail.com>
2024-09-12 22:53:01 +0200

    update the ts config file

--- src/TypescriptTypes/PrimitiveTypes/primitiveTypes.spec.ts
+++ src/TypescriptTypes/PrimitiveTypes/primitiveTypes.spec.ts
@@ -1,5 +1,8 @@
+import { aSampleStringValue } from "./primitiveTypes";
+
 describe('Primitive Types in Typescript', () => {
   it('should test the primitive types in Typescript', () => {
-    expect(true).toBe(true);
+    expect(aSampleStringValue).toEqual('A sample string value');
+    expect(typeof aSampleStringValue).toBe('string');
   });
 });
---
+++ src/TypescriptTypes/PrimitiveTypes/primitiveTypes.ts
@@ -0,0 +1,1 @@
+export const aSampleStringValue = 'A sample string value';
