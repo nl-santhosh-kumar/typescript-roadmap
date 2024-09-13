d4724fd76b22b7c4ee6c4daa8b176efcfc5943a9
santhoshkumarravichandran <santhoshkumar.ravi@gmail.com>
2024-09-13 08:56:27 +0200

    :fire: completing the primitive types chapter

--- src/chapters/1.TypescriptTypes/1.1PrimitiveTypes/primitiveTypes.spec.ts
+++ src/chapters/1.TypescriptTypes/1.1PrimitiveTypes/primitiveTypes.spec.ts
@@ -1,8 +1,21 @@
-import { aSampleStringValue } from "./primitiveTypes";
+import {  aStringValue, aBooleanFalseValue, aBooleanTrueValue, aNumberValue, anotherNumberValue, } from "./primitiveTypes";
 
 describe("Primitive Types in Typescript", () => {
   it("should test the primitive types in Typescript", () => {
-    expect(aSampleStringValue).toEqual("A sample string value");
-    expect(typeof aSampleStringValue).toBe("string");
+    expect(aStringValue).toEqual("A sample string value");
+    expect(typeof aStringValue).toBe("string");
+  });
+  it("should test the primitive types(boolean) in Typescript", () => {
+    expect(aBooleanTrueValue).toBeTruthy();
+    expect(typeof aBooleanTrueValue).toBe("boolean");
+    expect(aBooleanFalseValue).toBeFalsy();
+    expect(typeof aBooleanFalseValue).toBe("boolean");
+  });
+
+  it("should test the primitive types(number) in Typescript", () => {
+    expect(aNumberValue).toEqual(1000);
+    expect(typeof aNumberValue).toBe("number");
+    expect(anotherNumberValue).toEqual(3.14);
+    expect(typeof anotherNumberValue).toBe("number");
   });
 });
--- src/chapters/1.TypescriptTypes/1.1PrimitiveTypes/primitiveTypes.ts
+++ src/chapters/1.TypescriptTypes/1.1PrimitiveTypes/primitiveTypes.ts
@@ -1,1 +1,45 @@
-export const aSampleStringValue = "A sample string value";
+
+/**
+ * It is a primitive data type in TypeScript that represents textual data. 
+ * It is a set of elements of the 16-bit Unicode character set.
+ */
+export const aStringValue: string = "A sample string value";
+export const anotherStringValue: string = 'An another string value';
+
+/**
+ * It is a primitive data type in TypeScript that represents numeric values. 
+ * It includes both integer and floating-point values.
+ */
+export const aNumberValue: number = 1000;
+export const anotherNumberValue: number = 3.14;
+/**
+ * boolean is a primitive data type in TypeScript that 
+ * represents a boolean value i.e. either true or false.
+ * Given below is an example of a boolean variable declaration:
+ */
+export const aBooleanTrueValue: boolean = true;
+export const aBooleanFalseValue: boolean = false;
+
+/**
+ * void represents the return value of functions which don’t return a value. 
+ * It’s the inferred type any time a function doesn’t have any return statements, 
+ * or doesn’t return any explicit value from those return statements.
+ * 
+ * Note:
+ * In JavaScript, a function that doesn’t return any value will implicitly return 
+ * the value undefined. However, void and undefined are not the same thing in TypeScript.
+ */
+export function aVoidFunction():void {
+    return;
+}
+
+/**
+ * undefined represents that the value is uninitialized.
+ * A const variable has to be initialised, thus here its being initialised with undefined 
+ */
+export const anUndefinedVariable: undefined = undefined;
+
+/**
+ * null is a primitive value used to represent absent value.
+ */
+export const aNullValue: null = null;
